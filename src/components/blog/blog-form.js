f76166fa7ee9bc732004190835905fc0832ac630
jovar24 <jovar2497@gmail.com>
2019-10-21 22:51:30 -0600

    installed wysiwyg.scss

--- src/components/blog/blog-form.js
+++ src/components/blog/blog-form.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import axios from "axios";
+import RichTextEditor from "../forms/rich-text-editor";
 
 export default class BlogForm extends Component {
   constructor(props) {
@@ -7,11 +8,15 @@ export default class BlogForm extends Component {
 
     this.state = {
       title: "",
-      blog_status: ""
+      blog_status: "",
+      content: ""
     };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleRichTextEditorChange = this.handleRichTextEditorChange.bind(
+      this
+    );
   }
 
   buildForm() {
@@ -71,6 +76,10 @@ export default class BlogForm extends Component {
             placeholder="Blog status"
             value={this.state.blog_status}
           />
+        </div>
+
+        <div className="one-column">
+          <RichTextEditor />
         </div>
 
         <button className="btn">Save</button>
