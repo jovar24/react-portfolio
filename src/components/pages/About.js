9593309f73be61d2dcb1d0c48ca9834c9fb199c8
Jose Vargas <jovar2497@gmail.com>
2019-10-29 17:41:07 -0600

    pleeease

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import profilePic from "../../../static/assets/images/BIO/bio.jpg"
+import profilePic from "../../../static/assets/images/bio/bio.jpg"
 
 export default function () {
   return (
